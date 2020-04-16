adbd2af1e054be7698c9241ea3bf5018b86cb6ca
taogege <1340420560@qq.com>
2020-04-16 20:34:24 +0800

    统一异常处理

---
+++ src/main/java/com/mtli/controller/ErrorController.java
@@ -0,0 +1,40 @@
+package com.mtli.controller;
+
+import com.mtli.model.entity.Result;
+import com.mtli.model.entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class ErrorController {
+
+
+    public static final String FREQUENT_OPERATION = "/frequentOperation";
+
+    @Autowired
+    private HttpServletResponse response;
+
+    /**
+     * 有匹配的路径，但是路径下的资源不存在
+     * 如/img/xxx.jpg
+     *
+     * @return
+     */
+    @RequestMapping("/notfound")
+    public Result notfound() {
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        return Result.create(StatusCode.NOTFOUND, "文件不存在");
+    }
+
+
+    @RequestMapping(FREQUENT_OPERATION)
+    public Result frequentOperation() {
+
+        return Result.create(StatusCode.REPERROR, "操作过于频繁");
+    }
+
+
+}
